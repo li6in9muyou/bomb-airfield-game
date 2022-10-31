97db0317df21549710476ed2de526f838d1b126f
IDqian <e90c08@163.com>
2022-10-31 08:51:45 +0800

    更改了数据类

---
+++ ClassLibrary1/Airplane.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary1
+{
+    public class Airplane
+    {
+        private int x, y;
+        private  string direction = null;
+        //up down left right
+        public int X { get { return x; } set { x = value; } }
+        public int Y { get { return y; } set { y = value; } }
+        public string Direction { get { return direction; } set { direction = value; } }
+
+        Airplane(int x1, int y1, string direction1)
+        {
+            x= x1;
+            y= y1;
+            direction = direction1;
+        }
+    }
+}
