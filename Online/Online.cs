9c090ab0e582fe7002787e360cc50a1444daaba9
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-11-01 16:49:21 +0800

    尝试两个 api 来获取ip地址

--- Online/Online.cs
+++ Online/Online.cs
@@ -70,13 +70,26 @@ public class Online
 
     private static string FetchPublicIPv6Address()
     {
+        var apis = new Dictionary<string, Func<string, string>>
+        {
+            { "https://ip6only.me/api/", s => s.Split(",")[1] },
+            { "https://api-ipv6.ip.sb/ip", s => s }
+        };
         using var c = new HttpClient();
-        var t = c.GetStringAsync("https://ip6only.me/api/");
-        // todo: 如果这个地址访问不到怎么办，搞多几个这样的api作为后备。
-        t.Wait();
-        var ipv6Address = t.Result.Split(",")[1];
-        Console.Out.WriteLine("ipv6Address = {0}", ipv6Address);
-        return ipv6Address;
+        foreach (var (api, trans) in apis)
+            try
+            {
+                var t = c.GetStringAsync(api);
+                t.Wait();
+                var ipv6Address = trans(t.Result);
+                Console.Out.WriteLine("ipv6Address = {0}", ipv6Address);
+                return ipv6Address;
+            }
+            catch (HttpRequestException)
+            {
+            }
+
+        throw new Exception("can not get fetch ip address of this machine");
     }
 
     public string CreateRoom()
