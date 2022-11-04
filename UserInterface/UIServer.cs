df069d657c179aae947c6668db8789e335d7a92b
浮阳沫日 <2065646695@qq.com>
2022-11-04 23:30:23 +0800

    完成了界面类的wait系列操作

--- UserInterface/UIServer.cs
+++ UserInterface/UIServer.cs
@@ -1,9 +1,67 @@
+using Newtonsoft.Json;
+using Fleck;
 namespace UserInterface;
 
+public class MsgRoot
+{
+    public string header { get; set; }
+    public string body { get; set; }
+}
 public class UIServer
 {
-    public static void Init()
+    private static WebSocketServer? _server;
+    private static IWebSocketConnection? _socket;
+    public static void Init() 
     {
-        Console.WriteLine("cscscs");
+        _server = new WebSocketServer("ws://127.0.0.1:9898");
+        _server.Start(socket => {
+            socket.OnOpen = () =>
+            {
+                _socket = socket;
+                Console.WriteLine("Open");
+            };
+            socket.OnClose = () =>
+            {
+                _socket = null;
+                Console.WriteLine("Close");
+            };
+            socket.OnMessage = message => {
+                Console.WriteLine(message);//接收消息，判断类型，放入相应缓存
+                SaveMsg(message);
+            };
+        });
+        while (true) 
+        {
+            Console.Write("> ");
+            String? cmd = Console.ReadLine();
+            if (cmd!=null&&cmd.Equals("close"))break;
+        }
+    }
+    public static void SendMsg(String header,String body)
+    {
+        if (_socket != null)
+        {
+            //俩参数，一头一内容，在此合并
+            var data = new { header = header, body = body };
+            string jsonData = JsonConvert.SerializeObject(data);
+            _socket.Send(jsonData);
+        }
+    }
+    public static void SaveMsg(String msg)
+    {
+        if (_socket != null)
+        {
+            MsgRoot msgRoot = JsonConvert.DeserializeObject<MsgRoot>(msg);
+            if (msgRoot.header.Equals("IpAddress"))
+            {
+                UiCache.SetIpAddress(msgRoot.body);
+            }else if (msgRoot.header.Equals("BombLocation"))
+            {
+                UiCache.SetBombLocation(msgRoot.body);
+            }else if (msgRoot.header.Equals("AirplanesPlacement"))
+            {
+                UiCache.SetAirplanesPlacement(msgRoot.body);
+            }
+        }
     }
 }
--- UserInterface/UserInterfaceAdapter.cs
+++ UserInterface/UserInterfaceAdapter.cs
@@ -1,17 +1,48 @@
 ﻿using Common;
 using GameLogic;
+using Newtonsoft.Json;
+using System.Threading;
 
 namespace UserInterface;
-
+public class Ap
+{
+    public string direction { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+}
+public class ApRoot
+{
+    public List <Ap > Aps { get; set; }
+}
+public class CoorRoot
+{
+    public int x { get; set; }
+    public int y { get; set; }
+}
 public class UserInterfaceAdapter : IUserInterface
 {
     public AirplanePlacement[] WaitLocalUserPlaceAirplanes(GameLogic.GameLogic game)
     {
-        throw new NotImplementedException();
+        //未设置GameLogic状态
+        String JsonAP = UiCache.WaitAirplanesPlacement();
+        ApRoot apRoot = JsonConvert.DeserializeObject<ApRoot>(JsonAP);
+        AirplanePlacement[] aps = new AirplanePlacement[apRoot.Aps.Count];
+        int i = 0;
+        foreach (var ap in apRoot.Aps)
+        {
+            var airPlanePlacement = new AirplanePlacement();
+            airPlanePlacement.Direction = ap.direction;
+            var coordinate = new Coordinate(ap.x, ap.y);
+            airPlanePlacement.HeadCoord = coordinate;
+            aps[i++] = airPlanePlacement;
+        }
+        return aps;
     }
 
     public string WaitUserEnterAnIpAddress(string recommended)
     {
+        String ipAddress = UiCache.WaitIpAddress();
+        return ipAddress;
         throw new NotImplementedException();
     }
 
@@ -25,16 +56,15 @@ public class UserInterfaceAdapter : IUserInterface
 
     public Coordinate WaitLocalUserChooseBombLocation(GameLogic.GameLogic game)
     {
-        return Coordinate.Void();
+        //未设置GameLogic状态
+        String JsonCoor = UiCache.WaitBombLocation();
+        CoorRoot coorRoot = JsonConvert.DeserializeObject<CoorRoot>(JsonCoor);
+        Coordinate coordinate = new Coordinate(coorRoot.x, coorRoot.y);
+        return coordinate;
     }
 
     public AirplanePlacement[] WaitLocalUserPlaceAirplanes()
     {
         return Array.Empty<AirplanePlacement>();
-    }
-
-    public string WaitInputIpAddress()
-    {
-        return "";
     }
 }
