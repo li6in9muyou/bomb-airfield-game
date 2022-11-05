4f5c22a58bfa2108eff3eceda7f0b714edf0c4ac
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-11-05 21:01:21 +0800

    按照文档修改程序的全过程

--- BombAnAirplane/Program.cs
+++ BombAnAirplane/Program.cs
@@ -8,7 +8,7 @@ internal static class Program
 {
     private static void Main()
     {
-        // init ui
+        // 各子系统初始化
         IUserInterface ui = new ReplayUi("", new[]
         {
             new Coordinate(0, 0),
@@ -16,13 +16,10 @@ internal static class Program
             new Coordinate(2, 6),
             new Coordinate(3, 7)
         });
-        // init online
         ICommunicator communicator = new MockCommunicator();
         var online = new Online.Online(communicator);
-        // init game
-        var game = new GameLogic.GameLogic();
 
-        // 获取一个IP
+        // 询问玩家如何联机
         var ipAddress = ui.WaitUserEnterAnIpAddress("localhost");
 
         bool isIBombFirst;
@@ -35,36 +32,51 @@ internal static class Program
         }
         else
         {
-            // 我要加入别的房间
+            // 我要加入别人的房间
             isIBombFirst = online.WaitJoinOpponentRoom(ipAddress);
         }
 
         // 我是不是先手
         Console.Out.WriteLine("isIBombFirst = {0}", isIBombFirst);
 
-        ui.WaitLocalUserPlaceAirplanes(game);
-
-        online.WaitOpponentPlaceAirplane();
-
-        var isMyTurnToBomb = isIBombFirst;
-        while (!game.ShouldTerminate())
+        // 游戏主循环
+        do
         {
-            if (isMyTurnToBomb)
+            var game = new GameLogic.GameLogic();
+            ui.WaitLocalUserPlaceAirplanes(game);
+            online.WaitOpponentPlaceAirplane();
+            var isMyTurnToBomb = isIBombFirst;
+            while (true)
             {
-                var coordinate = ui.WaitLocalUserChooseBombLocation(game);
-                var result = online.BombOpponentAirfieldAndWaitResult(coordinate);
-                game.LogBombResultOnOpponentAirfield(coordinate, result);
-            }
-            else
-            {
-                ui.DrawAdditionalContent("需等待对方选定炸的位置");
-                var coordinate = online.WaitOpponentToBombMyAirfield();
-                var result = game.GetBombResultOnMyAirfield(coordinate);
-                online.SendBombResultOfMyAirfield(result);
-            }
+                if (isMyTurnToBomb)
+                {
+                    var coordinate = ui.WaitLocalUserChooseBombLocation(game);
+                    var result = online.BombOpponentAirfieldAndWaitResult(coordinate);
+                    game.LogBombResultOnOpponentAirfield(coordinate, result);
+                }
+                else
+                {
+                    ui.DrawAdditionalContent("需等待对方选定炸的位置");
+                    var coordinate = online.WaitOpponentToBombMyAirfield();
+                    var result = game.GetBombResultOnMyAirfield(coordinate);
+                    online.SendBombResultOfMyAirfield(result);
+                }
 
-            ui.DrawGameLogic(game);
-            isMyTurnToBomb = !isMyTurnToBomb;
-        }
+                ui.DrawGameLogic(game);
+                isMyTurnToBomb = !isMyTurnToBomb;
+
+                if (game.MyAirfieldIsDoomed())
+                {
+                    online.WaitSurrenderToOpponent();
+                    ui.DrawLocalUserLost();
+                    break;
+                }
+
+                online.WaitNotifyStillStanding();
+                if (!online.WaitOpponentToSurrender()) continue;
+                ui.DrawLocalUserWon();
+                break;
+            }
+        } while (ui.WaitLocalUserDecideWhetherToContinue());
     }
 }
