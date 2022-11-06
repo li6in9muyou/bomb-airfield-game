7670b3336e6712b9bc65c9df32bb73ecf0a0ed62
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-11-06 15:03:45 +0800

    改用日志工具而非控制台打印

--- BombAnAirplane/Program.cs
+++ BombAnAirplane/Program.cs
@@ -43,6 +43,7 @@ internal static class Program
         if (ipAddress == "")
         {
             // 我是房主
+            note.Debug("local creates a room");
             var remoteHandle = online.CreateRoom();
             ui.DrawAdditionalContent("本房间的IP地址是：" + remoteHandle);
             isIBombFirst = online.WaitOpponentToJoin();
@@ -50,29 +51,34 @@ internal static class Program
         else
         {
             // 我要加入别人的房间
+            note.Debug("local joins a room");
             isIBombFirst = online.WaitJoinOpponentRoom(ipAddress);
         }
 
         // 我是不是先手
-        Console.Out.WriteLine("isIBombFirst = {0}", isIBombFirst);
+        note.Debug($"hand shake result: isIBombFirst = {isIBombFirst}");
 
         // 游戏主循环
         do
         {
             var game = new GameLogic.GameLogic();
             ui.WaitLocalUserPlaceAirplanes(game);
+            note.Debug("local finishes placing airplanes");
             online.WaitOpponentPlaceAirplane();
+            note.Debug("remote finishes placing airplanes");
             var isMyTurnToBomb = isIBombFirst;
             while (true)
             {
                 if (isMyTurnToBomb)
                 {
+                    note.Debug("local is going to bomb");
                     var coordinate = ui.WaitLocalUserChooseBombLocation(game);
                     var result = online.BombOpponentAirfieldAndWaitResult(coordinate);
                     game.LogBombResultOnOpponentAirfield(coordinate, result);
                 }
                 else
                 {
+                    note.Debug("remote is going to bomb");
                     ui.DrawAdditionalContent("需等待对方选定炸的位置");
                     var coordinate = online.WaitOpponentToBombMyAirfield();
                     var result = game.GetBombResultOnMyAirfield(coordinate);
@@ -84,14 +90,25 @@ internal static class Program
 
                 if (game.MyAirfieldIsDoomed())
                 {
+                    note.Debug("local's airfield is doomed");
                     online.WaitSurrenderToOpponent();
                     ui.DrawLocalUserLost();
+
+                    note.Debug("this game is over");
                     break;
                 }
 
+                note.Debug("local not yet yields");
                 online.WaitNotifyStillStanding();
-                if (!online.WaitOpponentToSurrender()) continue;
+                if (!online.WaitOpponentToSurrender())
+                {
+                    note.Debug("remote not yet yields");
+                    continue;
+                }
+
+                note.Debug("local has won");
                 ui.DrawLocalUserWon();
+                note.Debug("this game is over");
                 break;
             }
         } while (ui.WaitLocalUserDecideWhetherToContinue());
