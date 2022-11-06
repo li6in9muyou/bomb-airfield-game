641b37abf7590ff09237526c0664d4ea31509353
li6in9muyou <libq5520@mails.jlu.edu.cn>
2022-11-06 14:21:53 +0800

    引入日志工具

---
+++ Common/Logging.cs
@@ -0,0 +1,15 @@
+﻿using Easy.Logger;
+using Easy.Logger.Interfaces;
+
+namespace Common;
+
+public class Logging
+{
+    private static readonly ILogService LogService = Log4NetService.Instance;
+
+    public static IEasyLogger GetLogger(string name)
+    {
+        LogService.Configure(new FileInfo("./log4net.config"));
+        return LogService.GetLogger(name);
+    }
+}
