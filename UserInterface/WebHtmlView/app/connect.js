dbeb0ec71b6b7dbfb8cd03085aeedbad261800ee
浮阳沫日 <2065646695@qq.com>
2022-11-08 22:11:45 +0800

    添加server类，制定connect的接口

--- UserInterface/WebHtmlView/app/connect.js
+++ UserInterface/WebHtmlView/app/connect.js
@@ -1,6 +1,22 @@
 let tool = {
-   getIP: function getIP(){
-    return 123
-   }
+    //初始化
+    connect:function connect(){
+        //这里要调用setMsg方法，就是能显示信息到界面的函数
+    },
+    sendIP: function sendIP(){
+        //需要参数
+    },
+    getYourAttack: function getYourAttack(){
+
+        return 123
+    },
+    sendMyAttack: function sendMyAttack(){
+        //需要参数
+        return true
+    },
+    sendMyPosition: function sendMyPosition(){
+        //需要参数
+        return true
+    }
 }
 export default tool
