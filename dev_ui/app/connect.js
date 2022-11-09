514549e68f8cd35d4cc0290c179d7bbda96f74a8
王艺鑫 <10733332+wang-yixin221116@user.noreply.gitee.com>
2022-11-09 13:50:02 +0000

    update dev_ui/app/connect.js.
    Signed-off-by: 王艺鑫 <10733332+wang-yixin221116@user.noreply.gitee.com>

--- dev_ui/app/connect.js
+++ dev_ui/app/connect.js
@@ -1,4 +1,7 @@
 let tool = {
+   connect(des){
+
+   },
    getYourAttack: function getYourAttack(){
       return 123//得到敌方的攻击位置和结果
    },
@@ -7,12 +10,6 @@ let tool = {
    },
    sendMyPosition: function sendMyPosition(value){
       return true//发送飞机布局位置
-   },
-   getYourReady(){
-      return true//获取对方是否摆放飞机成功
-   },
-   sendMyReady(){
-    //告诉后端已经摆放成功
    },
    myResult(des){
     //des是提示框元素对象 提示信息直接给它的innerText赋值就行
