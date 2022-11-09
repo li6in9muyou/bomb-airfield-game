781b53293b6837290cf35b4cac3371e9554ba328
王艺鑫 <10733332+wang-yixin221116@user.noreply.gitee.com>
2022-11-09 08:00:43 +0000

    update dev_ui/app/connect.js.
    Signed-off-by: 王艺鑫 <10733332+wang-yixin221116@user.noreply.gitee.com>

--- dev_ui/app/connect.js
+++ dev_ui/app/connect.js
@@ -1,13 +1,4 @@
 let tool = {
-   createRoom(){
-     //告诉后端本机已经创建房间,其他人输入IP地址可以加入
-   },
-   tellCreateStart(){
-    return true //告诉我是否有人加入房间
-   },
-   tellJoinStart(ip){
-    return true //传入要加入房间的ip 告诉我是否加入房间成功
-   },
    getYourAttack: function getYourAttack(){
       return 123//得到敌方的攻击位置和结果
    },
@@ -25,6 +16,9 @@ let tool = {
    },
    myResult(des){
     //des是提示框元素对象 提示信息直接给它的innerText赋值就行
+   },
+   sendMyIP(ip){
+   //创建房间ip为空字符串 加入房间为ip地址
    }
 }
 export default tool
